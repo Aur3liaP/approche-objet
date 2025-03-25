a78ada4c39064ba6d531074215f1860322d64795
Pic Aurélia <pic.aurelia@gmail.com>
2025-03-25 11:34:31 +0100

    TP 01 correction

--- src/main/java/entites2/Personne.java
+++ src/main/java/entites2/Personne.java
@@ -5,5 +5,5 @@ import entites.AdressePostale;
 public class Personne {
     public String nom;
     public String prenom;
-    public AdressePostale adressePostale;
+    public AdressePostale adresse;
 }
